9c83beb8c7851d1a834d64abfe7ac0ff3a665abd
idohyeon <ehgus5026@naver.com>
2024-11-29 23:37:11 +0900

    -MemberRepository

--- src/main/java/com/PetCare/repository/Member/MemberRepository.java
+++ src/main/java/com/PetCare/repository/Member/MemberRepository.java
@@ -3,5 +3,10 @@ package com.PetCare.repository.Member;
 import com.PetCare.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<List<Member>> findByLoginId(String loginId);
 }
