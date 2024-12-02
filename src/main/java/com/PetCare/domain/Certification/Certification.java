f1fb52113ccb44b7195e72c13bbf714c5c961de1
idohyeon <ehgus5026@naver.com>
2024-12-02 23:03:03 +0900

    -Certification Entity

---
+++ src/main/java/com/PetCare/domain/Certification/Certification.java
@@ -0,0 +1,39 @@
+package com.PetCare.domain.Certification;
+
+import com.PetCare.domain.Member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Certification {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "certification_id", updatable = false)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Comment("자격증 이름")
+    private String name;
+
+    @Builder
+    public Certification(String name) {
+        this.name = name;
+    }
+
+    // 돌봄사-자격증 연관관계 편의 메서드
+    public void addMember(Member member) {
+        this.member = member;
+        member.getCertifications().add(this);
+    }
+
+}
