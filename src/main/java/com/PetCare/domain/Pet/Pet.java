52710786d76cb7ad1eab37bf3dc9352ae1dc7ea7
idohyeon <ehgus5026@naver.com>
2024-12-02 16:18:16 +0900

    -Pet Entity

--- src/main/java/com/PetCare/domain/Pet/Pet.java
+++ src/main/java/com/PetCare/domain/Pet/Pet.java
@@ -36,7 +36,7 @@ public class Pet {
     private String profileImgUrl; // 반려견 프로필 사진
 
     // 고객-반려견 연관관계 편의 메서드
-    public void assignMember(Member member) {
+    public void addMember(Member member) {
         this.member = member;
         member.getPets().add(this);
     }
