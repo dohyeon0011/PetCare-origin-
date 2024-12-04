93233606e3e2c3fad535ed40bddafe604ae31179
idohyeon <ehgus5026@naver.com>
2024-12-04 21:03:00 +0900

    -Setting(+QueryDSL)

--- src/main/java/com/PetCare/PetCareApplication.java
+++ src/main/java/com/PetCare/PetCareApplication.java
@@ -1,7 +1,10 @@
 package com.PetCare;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class PetCareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetCareApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
