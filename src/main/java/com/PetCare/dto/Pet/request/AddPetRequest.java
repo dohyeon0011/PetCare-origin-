60a69e47c6f2cc4fef3382836db2f90c30212ca5
idohyeon <ehgus5026@naver.com>
2024-12-04 21:01:06 +0900

    -Pet Add DTO

--- src/main/java/com/PetCare/dto/Pet/request/AddPetRequest.java
+++ src/main/java/com/PetCare/dto/Pet/request/AddPetRequest.java
@@ -2,6 +2,7 @@ package com.PetCare.dto.Pet.request;
 
 import com.PetCare.domain.Pet.Pet;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class AddPetRequest {
 
     private String name;
 
-    @NotBlank(message = "반려견 나이 입력은 필수입니다.")
+    @NotNull(message = "반려견 나이 입력은 필수입니다.")
     private int age;
 
     @NotBlank(message = "반려견 품종 입력은 필수입니다.")
