d3424ca929f70ab9ead12ec02913d7f2ccb6c12d
idohyeon <ehgus5026@naver.com>
2024-12-04 21:02:32 +0900

    -Member View DTO

---
+++ src/main/java/com/PetCare/dto/Member/response/MemberResponse.java
@@ -0,0 +1,47 @@
+package com.PetCare.dto.Member.response;
+
+import com.PetCare.domain.Member.Member;
+import com.PetCare.domain.Member.Role;
+import com.PetCare.domain.Member.SocialProvider;
+import com.PetCare.domain.Pet.Pet;
+import com.PetCare.dto.Pet.response.PetResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Getter
+public class MemberResponse {
+    private String name;
+    private String nickName;
+    private String email;
+    private String phoneNumber;
+    private String address1;
+    private String address2;
+    private Role role;
+    private SocialProvider socialProvider;
+    private String introduction;
+    private List<PetResponse> pets = new ArrayList<>();
+    private Integer careerYear;
+    private List<String> certifications;
+
+    public MemberResponse(Member member, List<Pet> pets) {
+        this.name = member.getName();
+        this.nickName = member.getNickName();
+        this.email = member.getEmail();
+        this.phoneNumber = member.getPhoneNumber();
+        this.address1 = member.getAddress1();
+        this.address2 = member.getAddress2();
+        this.role = member.getRole();
+        this.socialProvider = member.getSocialProvider();
+        this.introduction = member.getIntroduction();
+        this.pets = pets.stream()
+                .map(PetResponse::new)
+                .collect(Collectors.toList());
+        this.careerYear = member.getCareerYear();
+        this.certifications = member.getCertifications();
+    }
+}
