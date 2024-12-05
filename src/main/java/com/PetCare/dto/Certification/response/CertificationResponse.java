f68d05da0467b6aace37760841417e930c3c27f4
idohyeon <ehgus5026@naver.com>
2024-12-05 20:17:10 +0900

    -Certification View DTO

---
+++ src/main/java/com/PetCare/dto/Certification/response/CertificationResponse.java
@@ -0,0 +1,17 @@
+package com.PetCare.dto.Certification.response;
+
+import com.PetCare.domain.Certification.Certification;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class CertificationResponse {
+
+    private String name;
+
+    public CertificationResponse(Certification certification) {
+        this.name = certification.getName();
+    }
+
+}
