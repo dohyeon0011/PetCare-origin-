3710e8565ae39983e4a81674c4900a2c4d5bbf70
idohyeon <ehgus5026@naver.com>
2024-12-05 21:15:19 +0900

    -Pet Service

--- src/main/java/com/PetCare/service/Pet/PetService.java
+++ src/main/java/com/PetCare/service/Pet/PetService.java
@@ -55,14 +55,18 @@ public class PetService {
     }
 
     @Transactional
-    public void delete(long id) {
-        List<Pet> pets = memberRepository.findPetsByMemberId(id);
+    public void delete(long id, List<Long> petIds) {
+        List<Pet> pets = petRepository.findByMemberIdAndIdIn(id, petIds);
 
         if (pets.isEmpty()) {
             throw new NoSuchElementException("삭제 오류: 등록된 반려견이 존재하지 않습니다.");
         }
 
-        petRepository.deleteAll(pets);
+        // 여러 개의 엔티티를 한 번의 쿼리로 삭제하는 방식으로 성능을 개선
+        // 이 방식은 영속성 컨텍스트(Persistence Context)에서 해당 엔티티들을 관리하지 않으므로, 엔티티 상태가 영속성 컨텍스트와 동기화되지 않음
+        // 즉, 삭제 후 해당 엔티티는 더 이상 영속성 컨텍스트에서 관리되지 않으며, 이후에 해당 엔티티에 접근하려면 다시 조회해야 함
+        petRepository.deleteAllInBatch(pets);
+//        petRepository.deleteAll(pets); // 얘는 여러번 쿼리 나감
     }
 
     @Transactional
