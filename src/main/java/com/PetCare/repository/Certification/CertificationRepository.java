00e70f32684c4b8d2a493f66dceb529fd717eea9
idohyeon <ehgus5026@naver.com>
2024-12-05 20:17:01 +0900

    -Certification Repository

---
+++ src/main/java/com/PetCare/repository/Certification/CertificationRepository.java
@@ -0,0 +1,7 @@
+package com.PetCare.repository.Certification;
+
+import com.PetCare.domain.Certification.Certification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertificationRepository extends JpaRepository<Certification, Long> {
+}
