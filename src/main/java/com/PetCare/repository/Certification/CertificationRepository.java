877c915f332c53ebf9291c5cc2df6a8842c17218
idohyeon <ehgus5026@naver.com>
2024-12-06 21:40:05 +0900

    -Certification Repository

--- src/main/java/com/PetCare/repository/Certification/CertificationRepository.java
+++ src/main/java/com/PetCare/repository/Certification/CertificationRepository.java
@@ -3,5 +3,15 @@ package com.PetCare.repository.Certification;
 import com.PetCare.domain.Certification.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
+
+    // 특정 회원의 자격증 목록 조회
+    List<Certification> findByMemberId(long memberId);
+
+    Certification findByMemberIdAndId(long memberId, long id);
+
+    // 특정 회원의 특정 자격증 목록 조회
+    List<Certification> findByMemberIdAndIdIn(long memberId, List<Long> ids);
 }
