92a26923f186d9290f88c668b5a171f7bc06396f
idohyeon <ehgus5026@naver.com>
2024-12-06 21:42:36 +0900

    -Add Certification DTO

--- src/main/java/com/PetCare/dto/Certification/request/AddCertificationRequest.java
+++ src/main/java/com/PetCare/dto/Certification/request/AddCertificationRequest.java
@@ -1,6 +1,7 @@
 package com.PetCare.dto.Certification.request;
 
 import com.PetCare.domain.Certification.Certification;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class AddCertificationRequest {
 
+    @NotBlank(message = "자격증 이름 입력은 필수입니다.")
     private String name;
 
     public Certification toEntity() {
