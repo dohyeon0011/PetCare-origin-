fe494fc1ff2a274ca5d76977c910a5dfe67ccc58
idohyeon <ehgus5026@naver.com>
2024-12-06 21:39:57 +0900

    -Certification Api Controller

---
+++ src/main/java/com/PetCare/controller/Certification/api/CertificationApiController.java
@@ -0,0 +1,11 @@
+package com.PetCare.controller.Certification.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CertificationApiController {
+
+
+}
