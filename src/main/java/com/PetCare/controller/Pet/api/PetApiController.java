0a9e6a407a2c5f66fc866e660ee674eaa39a4578
idohyeon <ehgus5026@naver.com>
2024-12-09 19:41:57 +0900

    -Pet Api Controller

--- src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
+++ src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
@@ -7,6 +7,7 @@ import com.PetCare.dto.Pet.response.PetResponse;
 import com.PetCare.service.Pet.PetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,14 +21,16 @@ public class PetApiController {
 
     private final PetService petService;
 
+    @Comment("반려견 등록")
     @PostMapping("/{memberId}/pets/new")
-    public ResponseEntity<List<Pet>> addPet(@PathVariable("memberId") long id, @RequestBody @Valid List<AddPetRequest> request) {
-        List<Pet> pets = petService.save(id, request);
+    public ResponseEntity<List<Pet>> addPet(@PathVariable("memberId") long id, @RequestBody @Valid List<AddPetRequest> requests) {
+        List<Pet> pets = petService.save(id, requests);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(pets);
     }
 
+    @Comment("특정 회원의 모든 반려견 조회")
     @GetMapping("/{memberId}/pets")
     public ResponseEntity<List<PetResponse>> findPets(@PathVariable("memberId") long id) {
         List<PetResponse> pets = petService.findById(id);
@@ -36,6 +39,7 @@ public class PetApiController {
                 .body(pets);
     }
 
+    @Comment("특정 회원의 특정 반려견 삭제")
     @DeleteMapping("{memberId}/pets/{petId}")
     public ResponseEntity<Void> deletePet(@PathVariable("memberId") long id, @PathVariable("petId") long petId) {
         petService.delete(id, petId);
@@ -44,6 +48,7 @@ public class PetApiController {
                 .build();
     }
 
+    @Comment("특정 회원의 반려견 정보 수정")
     @PutMapping("{memberId}/pets")
     public ResponseEntity<List<PetResponse>> updatePet(@PathVariable("memberId") long id, @RequestBody @Valid List<UpdatePetRequest> requests) {
         List<PetResponse> updatePets = petService.update(id, requests);
