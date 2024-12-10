fc87e8bcdac9aeae2ee21b504178804284607dba
idohyeon <ehgus5026@naver.com>
2024-12-10 18:49:15 +0900

    -Add CareAvailability DTO

---
+++ src/main/java/com/PetCare/dto/CareAvailability/request/AddCareAvailabilityRequest.java
@@ -0,0 +1,36 @@
+package com.PetCare.dto.CareAvailability.request;
+
+import com.PetCare.domain.CareAvailability.CareAvailability;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddCareAvailabilityRequest {
+
+    @NotNull(message = "날짜 입력은 필수입니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd") // DTO는 뷰와 컨트롤러 간 데이터 처리에 집중
+    private LocalDate availabilityAt;
+
+    @NotNull(message = "돌봄 비용 입력은 필수입니다.")
+    private int price;
+
+    public CareAvailability toEntity() {
+        // 수동 타입 변환
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        LocalDate date = LocalDate.parse(availabilityAt, formatter);
+
+        // <input type="date" th:field="*{date}" /> 타임리프 폼에서 이런 식으로 받으면 자동으로 스프링이 변환해줌
+        return CareAvailability.builder()
+                .availabilityAt(availabilityAt)
+                .price(price)
+                .build();
+    }
+
+}
