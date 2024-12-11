5e487e38ea3e6df40b072e94ad0c7f4a223820d5
idohyeon <ehgus5026@naver.com>
2024-12-11 18:37:44 +0900

    -CareAvailableDate Repository

---
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
@@ -0,0 +1,16 @@
+package com.PetCare.repository.CareAvailableDate;
+
+import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CareAvailableDateRepository extends JpaRepository<CareAvailableDate, Long> {
+
+    // 특정 회원의 돌봄 가능한 날짜 모두 조회
+    List<CareAvailableDate> findByMemberId(long memberId);
+
+    // 특정 회원의 특정 돌봄 가능한 날짜 조회
+    Optional<CareAvailableDate> findByMemberIdAndId(long memberId, long id);
+}
