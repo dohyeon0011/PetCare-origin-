a4c65888581a845c0de61863501e2db19889792b
idohyeon <ehgus5026@naver.com>
2024-12-11 18:38:07 +0900

    -CareAvailableDate View DTO

---
+++ src/main/java/com/PetCare/dto/CareAvailableDate/response/CareAvailableDateResponse.java
@@ -0,0 +1,25 @@
+package com.PetCare.dto.CareAvailableDate.response;
+
+import com.PetCare.domain.CareAvailableDate.CareAvailableDateStatus;
+import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+public class CareAvailableDateResponse {
+    private long id;
+    private LocalDate availabilityAt;
+    private int price;
+    private CareAvailableDateStatus status;
+
+    public CareAvailableDateResponse(CareAvailableDate careAvailableDate) {
+        this.id = careAvailableDate.getId();
+        this.availabilityAt = careAvailableDate.getAvailabilityAt();
+        this.price = careAvailableDate.getPrice();
+        this.status = careAvailableDate.getStatus();
+    }
+
+}
