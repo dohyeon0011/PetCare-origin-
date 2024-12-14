e86415a393760ca239ee43fa357345b99e1cc0d8
idohyeon <ehgus5026@naver.com>
2024-12-15 01:29:00 +0900

    -CustomerReservation View DTO(예약 정보 조회, 고객시점)

--- src/main/java/com/PetCare/dto/CustomerReservation/response/CustomerReservationResponse.java
+++ src/main/java/com/PetCare/dto/CustomerReservation/response/CustomerReservationResponse.java
@@ -1,8 +1,9 @@
 package com.PetCare.dto.CustomerReservation.response;
 
 import com.PetCare.domain.CustomerReservation.CustomerReservation;
+import com.PetCare.domain.CustomerReservation.PetReservation;
 import com.PetCare.domain.CustomerReservation.ReservationStatus;
-import com.PetCare.domain.Pet.Pet;
+import com.PetCare.domain.Member.Member;
 import com.PetCare.dto.Pet.response.PetReservationResponse;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,33 +13,51 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@NoArgsConstructor
-@Getter
+
 public class CustomerReservationResponse {
-    private long id;
-    private String nickName;
-    private LocalDate reservationAt;
-    private LocalDateTime createdAt;
-    private ReservationStatus status;
-    private List<PetReservationResponse> petReservations;
 
-    // 전체 조회
-    public CustomerReservationResponse(long id, String nickName, CustomerReservation customerReservation) {
-        this.id = id;
-        this.nickName = nickName;
-        this.reservationAt = customerReservation.getReservationAt();
-        this.createdAt = customerReservation.getCreatedAt();
-        this.status = customerReservation.getStatus();
+    @NoArgsConstructor
+    @Getter
+    public static class GetList {
+        private LocalDate reservationAt;
+        private LocalDateTime createdAt;
+        private ReservationStatus status;
+
+        public GetList(CustomerReservation customerReservation) {
+            this.reservationAt = customerReservation.getReservationAt();
+            this.createdAt = customerReservation.getCreatedAt();
+            this.status = customerReservation.getStatus();
+        }
     }
 
-    // 예약 상세 조회
-    public CustomerReservationResponse(CustomerReservation customerReservation, List<Pet> pets) {
-        this.reservationAt = customerReservation.getReservationAt();
-        this.createdAt = customerReservation.getCreatedAt();
-        this.status = customerReservation.getStatus();
-        this.petReservations = pets
-                .stream()
-                .map(PetReservationResponse::new)
-                .collect(Collectors.toList());
+    @NoArgsConstructor
+    @Getter
+    public static class GetDetail {
+        private long id;
+        private long customerId;
+        private String customerNickName;
+        private long sitterId;
+        private String sitterNickName;
+        private int price;
+        private LocalDate reservationAt;
+        private LocalDateTime createdAt;
+        private ReservationStatus status;
+        private List<PetReservationResponse> petReservations;
+
+        public GetDetail(Member customer, Member sitter, CustomerReservation customerReservation, List<PetReservation> pets) {
+            this.id = customerReservation.getId();
+            this.customerId = customer.getId();
+            this.customerNickName = customer.getNickName();
+            this.sitterId = sitter.getId();
+            this.sitterNickName = sitter.getNickName();
+            this.reservationAt = customerReservation.getReservationAt();
+            this.createdAt = customerReservation.getCreatedAt();
+            this.status = customerReservation.getStatus();
+            this.petReservations = pets
+                    .stream()
+                    .map(PetReservationResponse::new)
+                    .collect(Collectors.toList());
+        }
     }
+
 }
