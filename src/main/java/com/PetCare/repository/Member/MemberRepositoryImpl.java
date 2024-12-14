f98839d8c1f251a6f07787475b9d14879109cc0b
idohyeon <ehgus5026@naver.com>
2024-12-15 01:30:21 +0900

    -Member ImplRepository(회원)

--- src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
+++ src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
@@ -1,20 +1,24 @@
 package com.PetCare.repository.Member;
 
+import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
 import com.PetCare.domain.Pet.Pet;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
+import static com.PetCare.domain.CareAvailableDate.QCareAvailableDate.careAvailableDate;
 import static com.PetCare.domain.Pet.QPet.pet;
 
+@RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
-    public MemberRepositoryImpl(JPAQueryFactory queryFactory) {
-        this.queryFactory = queryFactory;
-    }
-
+    // 고객이 보유한 반려견 목록 전체 조회
     @Override
     public List<Pet> findPetsByMemberId(long memberId) {
         return queryFactory
@@ -24,4 +28,23 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     }
 
+    // 돌봄사가 등록한 예약 가능 날짜 전체 조회
+    public List<CareAvailableDate> findCareAvailableDatesByMemberId(long memberId) {
+        LocalDateTime now = LocalDateTime.now();
+
+        return queryFactory
+                .selectFrom(careAvailableDate)
+                .where(careAvailableDate.member.id.eq(memberId)
+                        .and(careAvailableDate.availableAt.after(LocalDate.from(now))))
+                .fetch();
+    }
+
+    // 돌봄사가 등록한 예약 가능 날짜 중 특정 날짜 조회
+    public Optional<CareAvailableDate> findCareAvailableDatesByMemberIdAndAvailableAt(long memberId, LocalDate availableDate) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(careAvailableDate)
+                .where(careAvailableDate.member.id.eq(memberId)
+                        .and(careAvailableDate.availableAt.eq(availableDate)))
+                .fetchOne());
+    }
 }
