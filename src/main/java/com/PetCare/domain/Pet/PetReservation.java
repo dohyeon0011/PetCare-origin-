32380772d679abffc7e0d8b5588212d801a85527
idohyeon <ehgus5026@naver.com>
2024-12-17 01:18:36 +0900

    -PetReservation Mapping Entity(돌봄 배정 반려견)

---
+++ src/main/java/com/PetCare/domain/Pet/PetReservation.java
@@ -0,0 +1,58 @@
+package com.PetCare.domain.Pet;
+
+import com.PetCare.domain.CustomerReservation.CustomerReservation;
+import com.PetCare.domain.SitterSchedule.SitterSchedule;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class PetReservation {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "pet_reservation_id")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id")
+    @JsonIgnore
+    private Pet pet;
+
+    @Comment("고객 시점 예약")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_reservation_id")
+    @JsonIgnore
+    private CustomerReservation customerReservation;
+
+    @Comment("돌봄사 시점 예약")
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "sitter_schedule_id")
+    @JsonIgnore
+    private SitterSchedule sitterSchedule;
+
+    public static PetReservation createPetReservation(Pet pet) {
+        PetReservation petReservation = new PetReservation();
+        petReservation.addPet(pet);
+
+        return petReservation;
+    }
+
+    public void addPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void addCustomerReservation(CustomerReservation customerReservation) {
+        this.customerReservation = customerReservation;
+    }
+
+    public void addSitterSchedule(SitterSchedule sitterSchedule) {
+        this.sitterSchedule = sitterSchedule;
+    }
+
+}
