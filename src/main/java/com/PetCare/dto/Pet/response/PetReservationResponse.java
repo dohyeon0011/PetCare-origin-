139e2c8f0a95576756e88d7155e922ebc2f83d9b
idohyeon <ehgus5026@naver.com>
2024-12-17 01:18:54 +0900

    -PetReservation DTO(돌봄 배정 반려견)

--- src/main/java/com/PetCare/dto/Pet/response/PetReservationResponse.java
+++ src/main/java/com/PetCare/dto/Pet/response/PetReservationResponse.java
@@ -1,12 +1,12 @@
 package com.PetCare.dto.Pet.response;
 
-import com.PetCare.domain.CustomerReservation.PetReservation;
+import com.PetCare.domain.Pet.PetReservation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class PetReservationResponse {
+public class PetReservationResponse { // 고객 시점 예약 엔티티 - 반려견 중간 매핑 엔티티
     private String name;
     private int age;
     private String breed;
