c1396a38edaf097dfdf3efb1f822627d55d41452
idohyeon <ehgus5026@naver.com>
2024-12-17 20:14:55 +0900

    -Pet API Controller(반려견)

--- src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
+++ src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
@@ -22,8 +22,8 @@ public class PetApiController {
     private final PetService petService;
 
     @Operation(description = "반려견 등록 API")
-    @PostMapping("/{memberId}/pets/new")
-    public ResponseEntity<List<Pet>> addPet(@PathVariable("memberId") long id, @RequestBody @Valid List<AddPetRequest> requests) {
+    @PostMapping("/{customerId}/pets/new")
+    public ResponseEntity<List<Pet>> addPet(@PathVariable("customerId") long id, @RequestBody @Valid List<AddPetRequest> requests) {
         List<Pet> pets = petService.save(id, requests);
 
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -31,8 +31,8 @@ public class PetApiController {
     }
 
     @Operation(description = "특정 회원의 모든 반려견 조회 API")
-    @GetMapping("/{memberId}/pets")
-    public ResponseEntity<List<PetResponse>> findPets(@PathVariable("memberId") long id) {
+    @GetMapping("/{customerId}/pets")
+    public ResponseEntity<List<PetResponse>> findPets(@PathVariable("customerId") long id) {
         List<PetResponse> pets = petService.findById(id);
 
         return ResponseEntity.ok()
@@ -40,8 +40,8 @@ public class PetApiController {
     }
 
     @Operation(description = "회원의 특정 반려견 삭제 API")
-    @DeleteMapping("{memberId}/pets/{petId}")
-    public ResponseEntity<Void> deletePet(@PathVariable("memberId") long id, @PathVariable("petId") long petId) {
+    @DeleteMapping("{customerId}/pets/{petId}")
+    public ResponseEntity<Void> deletePet(@PathVariable("customerId") long id, @PathVariable("petId") long petId) {
         petService.delete(id, petId);
 
         return ResponseEntity.ok()
@@ -49,8 +49,8 @@ public class PetApiController {
     }
 
     @Operation(description = "회원의 반려견 정보 수정 API")
-    @PutMapping("{memberId}/pets")
-    public ResponseEntity<List<PetResponse>> updatePet(@PathVariable("memberId") long id, @RequestBody @Valid List<UpdatePetRequest> requests) {
+    @PutMapping("{customerId}/pets")
+    public ResponseEntity<List<PetResponse>> updatePet(@PathVariable("customerId") long id, @RequestBody @Valid List<UpdatePetRequest> requests) {
         List<PetResponse> updatePets = petService.update(id, requests);
 
         return ResponseEntity.ok()
