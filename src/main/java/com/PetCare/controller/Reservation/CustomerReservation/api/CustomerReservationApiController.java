d91054def0cf1f9ecbd9883cffc475a60e759666
idohyeon <ehgus5026@naver.com>
2024-12-17 20:16:46 +0900

    -CustomerReservation API Controller(예약, 고객시점)

--- src/main/java/com/PetCare/controller/Reservation/CustomerReservation/api/CustomerReservationApiController.java
+++ src/main/java/com/PetCare/controller/Reservation/CustomerReservation/api/CustomerReservationApiController.java
@@ -1,8 +1,8 @@
 package com.PetCare.controller.Reservation.CustomerReservation.api;
 
 import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
-import com.PetCare.dto.Reservation.request.AddCustomerReservationRequest;
-import com.PetCare.dto.Reservation.response.ReservationResponse;
+import com.PetCare.dto.Reservation.CustomerReservation.request.AddCustomerReservationRequest;
+import com.PetCare.dto.Reservation.CustomerReservation.response.CustomerReservationResponse;
 import com.PetCare.service.Reservation.CustomerReservation.CustomerReservationService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -15,14 +15,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/PetsCare")
+@RequestMapping("/api/PetsCare/customer")
 public class CustomerReservationApiController {
 
     private final CustomerReservationService customerReservationService;
 
     @Operation(description = "회원 돌봄 예약 생성 API")
     @PostMapping("/reservation/new")
-    public ResponseEntity<CustomerReservation> saveCustomerReservation(@RequestBody @Valid AddCustomerReservationRequest request) {
+    public ResponseEntity<CustomerReservation> saveReservation(@RequestBody @Valid AddCustomerReservationRequest request) {
         CustomerReservation customerReservation = customerReservationService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -30,27 +30,27 @@ public class CustomerReservationApiController {
     }
 
     @Operation(description = "회원의 모든 돌봄 예약 내역 조회 API")
-    @GetMapping("/{memberId}/reservationList")
-    public ResponseEntity<List<ReservationResponse.GetList>> findCustomerReservationList(@PathVariable("memberId") long id) {
-        List<ReservationResponse.GetList> customerReservationList = customerReservationService.findAllById(id);
+    @GetMapping("/{customerId}/reservationList")
+    public ResponseEntity<List<CustomerReservationResponse.GetList>> findCustomerReservationList(@PathVariable("customerId") long id) {
+        List<CustomerReservationResponse.GetList> customerReservationList = customerReservationService.findAllById(id);
 
         return ResponseEntity.ok()
                 .body(customerReservationList);
     }
 
     @Operation(description = "회원의 특정 돌봄 예약 상세 조회 API")
-    @GetMapping("/{memberId}/reservation/{customerReservationId}")
-    public ResponseEntity<ReservationResponse.GetDetail> findCustomerReservationOne(@PathVariable("memberId") long id,
-                                                                                    @PathVariable("customerReservationId") long customerReservationId) {
-        ReservationResponse.GetDetail customerReservation = customerReservationService.findById(id, customerReservationId);
+    @GetMapping("/{customerId}/reservation/{customerReservationId}")
+    public ResponseEntity<CustomerReservationResponse.GetDetail> findCustomerReservation(@PathVariable("customerId") long id,
+                                                                                            @PathVariable("customerReservationId") long customerReservationId) {
+        CustomerReservationResponse.GetDetail customerReservation = customerReservationService.findById(id, customerReservationId);
 
         return ResponseEntity.ok()
                 .body(customerReservation);
     }
 
     @Operation(description = "특정 회원의 특정 돌봄 예약 취소 API")
-    @DeleteMapping("/{memberId}/reservation/{customerReservationId}")
-    public ResponseEntity<Void> deleteCustomerReservation(@PathVariable("memberId") long id,
+    @DeleteMapping("/{customerId}/reservation/{customerReservationId}")
+    public ResponseEntity<Void> deleteCustomerReservation(@PathVariable("customerId") long id,
                                                           @PathVariable("customerReservationId") long customerReservationId) {
         customerReservationService.delete(id, customerReservationId);
 
