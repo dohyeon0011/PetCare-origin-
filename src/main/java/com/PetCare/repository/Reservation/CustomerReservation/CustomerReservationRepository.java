408c46f92e4f2f29fe671496ce2d7ada05a2484b
idohyeon <ehgus5026@naver.com>
2024-12-17 20:16:28 +0900

    -CustomerReservation Repository(예약, 고객시점)

--- src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
+++ src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
@@ -8,9 +8,12 @@ import java.util.Optional;
 
 public interface CustomerReservationRepository extends JpaRepository<CustomerReservation, Long> {
 
-    // 특정 회원의 예약 내역 전체 조회
-    List<CustomerReservation> findByCustomerId(long customer);
+    // 특정 회원의 돌봄 예약 내역 전체 조회
+    List<CustomerReservation> findByCustomerId(long customerId);
 
-    // 특정 회원의 특정 예약 내역 조회
+    // 고객의 번호로 특정 회원의 특정 돌봄 예약 내역 조회
     Optional<CustomerReservation> findByCustomerIdAndId(long customerId, long id);
+
+    // 돌봄사의 예약 번호로 특정 회원의 특정 돌봄 예약 내역 조회
+//    Optional<CustomerReservation> findBySitterScheduleId(long sitterScheduleId);
 }
