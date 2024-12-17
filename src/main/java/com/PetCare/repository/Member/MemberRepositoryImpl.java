a6c75326fe6140736c3ad094291f55d4ecc4f335
idohyeon <ehgus5026@naver.com>
2024-12-17 20:15:17 +0900

    -Member Repository Impl(회원)

--- src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
+++ src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
@@ -19,32 +19,31 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     // 고객이 보유한 반려견 목록 전체 조회
-    @Override
-    public List<Pet> findPetsByMemberId(long memberId) {
+    public List<Pet> findPetsByCustomerId(long customerId) {
         return queryFactory
                 .selectFrom(pet)
-                .where(pet.member.id.eq(memberId))
+                .where(pet.customer.id.eq(customerId))
                 .fetch();
 
     }
 
     // 돌봄사가 등록한 예약 가능 날짜 전체 조회
-    public List<CareAvailableDate> findCareAvailableDatesByMemberId(long memberId) {
+    public List<CareAvailableDate> findCareAvailableDatesByCustomerId(long customerId) {
         LocalDateTime now = LocalDateTime.now();
 
         return queryFactory
                 .selectFrom(careAvailableDate)
-                .where(careAvailableDate.member.id.eq(memberId)
+                .where(careAvailableDate.sitter.id.eq(customerId)
                         .and(careAvailableDate.availableAt.after(LocalDate.from(now))))
                 .fetch();
     }
 
     // 돌봄사가 등록한 예약 가능 날짜 중 특정 날짜 조회
-    public Optional<CareAvailableDate> findCareAvailableDateByMemberIdAndAvailableAt(long memberId, LocalDate availableDate) {
+    public Optional<CareAvailableDate> findCareAvailableDateByCustomerIdAndAvailableAt(long customerId, LocalDate availableDate) {
         return Optional.ofNullable(
                 queryFactory
                         .selectFrom(careAvailableDate)
-                        .where(careAvailableDate.member.id.eq(memberId)
+                        .where(careAvailableDate.sitter.id.eq(customerId)
                                 .and(careAvailableDate.availableAt.eq(availableDate)))
                         .fetchOne());
     }
