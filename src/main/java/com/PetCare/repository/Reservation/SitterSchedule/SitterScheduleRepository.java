c913ecb89b7334f5546e3c16cfe272d3f93f09b1
idohyeon <ehgus5026@naver.com>
2024-12-17 20:13:46 +0900

    -SitterSchedule Repository(돌봄사 시점, 예약)

--- src/main/java/com/PetCare/repository/Reservation/SitterSchedule/SitterScheduleRepository.java
+++ src/main/java/com/PetCare/repository/Reservation/SitterSchedule/SitterScheduleRepository.java
@@ -4,10 +4,17 @@ import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
 import com.PetCare.domain.Reservation.SitterSchedule.SitterSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SitterScheduleRepository extends JpaRepository<SitterSchedule, Long> {
 
-    // 돌봄사에게 배정됐던 특정 예약 조회
+    // 돌봄사에게 배정됐던 모든 예약 조회
+    List<SitterSchedule> findBySitterId(long sitterId);
+
+    // 돌봄사의 특정 돌봄 예약 조회
+    Optional<SitterSchedule> findBySitterIdAndId(long sitterId, long id);
+
+    // 고객 예약으로 돌봄사에게 배정됐던 특정 예약 조회
     Optional<SitterSchedule> findByCustomerReservation(CustomerReservation customerReservation);
 }
