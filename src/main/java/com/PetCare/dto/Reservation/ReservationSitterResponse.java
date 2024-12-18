5046138d72e8f8a6cf27bb768e795dcdcb507f4d
idohyeon <ehgus5026@naver.com>
2024-12-18 18:32:14 +0900

    -Reservation Sitter DTO(고객시점 예약하기 전 돌봄사 정보 조회)

---
+++ src/main/java/com/PetCare/dto/Reservation/ReservationSitterResponse.java
@@ -0,0 +1,47 @@
+package com.PetCare.dto.Reservation;
+
+import com.PetCare.domain.Member.Member;
+import com.PetCare.dto.Certification.response.CertificationResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class ReservationSitterResponse { // 고객이 예약하기 전 보여줄 정보
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetList { // 예약 가능 목록에 있는 돌봄사들의 정보
+        private long sitterId;
+        private String sitterName;
+        private String introduction;
+
+        public GetList(Member sitter) {
+            this.sitterId = sitter.getId();
+            this.sitterName = sitter.getName();
+            this.introduction = sitter.getIntroduction();
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetDetail { // 예약 가능 목록 중 특정 돌봄사의 자세한 정보
+        private long sitterId;
+        private String sitterName;
+        private String introduction;
+        private Integer careerYear;
+        private List<CertificationResponse.GetReservation> certifications;
+
+        public GetDetail(Member sitter) {
+            this.sitterId = sitter.getId();
+            this.sitterName = sitter.getName();
+            this.introduction = sitter.getIntroduction();
+            this.careerYear = sitter.getCareerYear();
+            this.certifications = sitter.getCertifications()
+                    .stream()
+                    .map(CertificationResponse.GetReservation::new)
+                    .toList();
+        }
+    }
+
+}
