241cf57c5db3db10f699b100821a04a588a1d0c8
idohyeon <ehgus5026@naver.com>
2024-12-18 18:27:10 +0900

    -Member Repository Impl(회원)

--- src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
+++ src/main/java/com/PetCare/repository/Member/MemberRepositoryImpl.java
@@ -28,22 +28,22 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     }
 
     // 돌봄사가 등록한 예약 가능 날짜 전체 조회
-    public List<CareAvailableDate> findCareAvailableDatesByCustomerId(long customerId) {
+    public List<CareAvailableDate> findCareAvailableDatesBySitterId(long sitterId) {
         LocalDateTime now = LocalDateTime.now();
 
         return queryFactory
                 .selectFrom(careAvailableDate)
-                .where(careAvailableDate.sitter.id.eq(customerId)
+                .where(careAvailableDate.sitter.id.eq(sitterId)
                         .and(careAvailableDate.availableAt.after(LocalDate.from(now))))
                 .fetch();
     }
 
     // 돌봄사가 등록한 예약 가능 날짜 중 특정 날짜 조회
-    public Optional<CareAvailableDate> findCareAvailableDateByCustomerIdAndAvailableAt(long customerId, LocalDate availableDate) {
+    public Optional<CareAvailableDate> findCareAvailableDateBySitterIdAndAvailableAt(long sitterId, LocalDate availableDate) {
         return Optional.ofNullable(
                 queryFactory
                         .selectFrom(careAvailableDate)
-                        .where(careAvailableDate.sitter.id.eq(customerId)
+                        .where(careAvailableDate.sitter.id.eq(sitterId)
                                 .and(careAvailableDate.availableAt.eq(availableDate)))
                         .fetchOne());
     }
