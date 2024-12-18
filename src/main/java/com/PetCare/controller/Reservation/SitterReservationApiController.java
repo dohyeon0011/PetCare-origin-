230af92008844d074d56c9776f52c42500892c10
idohyeon <ehgus5026@naver.com>
2024-12-18 18:33:18 +0900

    -Sitter Reservation API Controller(고객시점 예약하기 전,후 조회될 정보)

---
+++ src/main/java/com/PetCare/controller/Reservation/SitterReservationApiController.java
@@ -0,0 +1,50 @@
+package com.PetCare.controller.Reservation;
+
+import com.PetCare.dto.Reservation.ReservationResponse;
+import com.PetCare.dto.Reservation.ReservationSitterResponse;
+import com.PetCare.service.Reservation.SitterReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/PetsCare")
+public class SitterReservationApiController {
+
+    private final SitterReservationService sitterReservationService;
+
+    @Operation(description = "고객에게 돌봄 예약 가능한 돌봄사들의 정보 조회 API")
+    @GetMapping("/reservableList")
+    public ResponseEntity<List<ReservationSitterResponse.GetList>> findAvailableReservationList() {
+        List<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters();
+
+        return ResponseEntity.ok()
+                .body(reservableSitters);
+    }
+
+    @Operation(description = "돌봄 예약 가능 목록 중 선택한 돌봄사의 자세한 정보 조회 API")
+    @GetMapping("/reservableList/{sitterId}")
+    public ResponseEntity<ReservationSitterResponse.GetDetail> findReservationSitter(@PathVariable("sitterId") long sitterId) {
+        ReservationSitterResponse.GetDetail reservableSitter = sitterReservationService.findReservableSitter(sitterId);
+
+        return ResponseEntity.ok()
+                .body(reservableSitter);
+    }
+
+    @Operation(description = "고객이 예약할 때 보여줄 정보 API")
+    @GetMapping("/reservation/members/{customerId}/sitter/{sitterId}")
+    public ResponseEntity<ReservationResponse> getReservationDetails(@PathVariable("customerId") long customerId,
+                                                                     @PathVariable("sitterId") long sitterId) {
+        ReservationResponse reservationDetails = sitterReservationService.getReservationDetails(customerId, sitterId);
+
+        return ResponseEntity.ok()
+                .body(reservationDetails);
+    }
+}
