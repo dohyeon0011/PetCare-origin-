460eb46548390a799ed39bfaa1b53dc7c3a623f5
idohyeon <ehgus5026@naver.com>
2024-12-18 18:29:50 +0900

    -Reservation View DTO(예약 시 노출될 정보)

---
+++ src/main/java/com/PetCare/dto/Reservation/ReservationResponse.java
@@ -0,0 +1,34 @@
+package com.PetCare.dto.Reservation;
+
+import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
+import com.PetCare.domain.Member.Member;
+import com.PetCare.domain.Pet.Pet;
+import com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse;
+import com.PetCare.dto.Pet.response.PetResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class ReservationResponse { // 고객이 예약할 때 보여줄 정보
+    private long sitterId;
+    private String sitterName;
+    private List<CareAvailableDateResponse.GetReservation> careAvailableDates;
+    private List<PetResponse.GetReservation> pets;
+
+    public ReservationResponse(Member sitter, List<CareAvailableDate> careAvailableDates, List<Pet> pets) {
+        this.sitterId = sitter.getId();
+        this.sitterName = sitter.getName();
+        this.careAvailableDates = careAvailableDates
+                .stream()
+                .map(CareAvailableDateResponse.GetReservation::new)
+                .toList();
+        this.pets = pets
+                .stream()
+                .map(PetResponse.GetReservation::new)
+                .toList();
+    }
+
+}
