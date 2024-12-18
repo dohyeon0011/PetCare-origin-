3caa786ea93d5bdea18d75bf5c219b402a0cc094
idohyeon <ehgus5026@naver.com>
2024-12-18 18:27:46 +0900

    -Pet View DTO(반려견 정보 조회)

--- src/main/java/com/PetCare/dto/Pet/response/PetResponse.java
+++ src/main/java/com/PetCare/dto/Pet/response/PetResponse.java
@@ -4,22 +4,41 @@ import com.PetCare.domain.Pet.Pet;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Getter
-public class PetResponse {
-    private long id;
-    private String name;
-    private int age;
-    private String breed;
-    private String medicalConditions;
-    private String profileImgUrl;
 
-    public PetResponse(Pet pet) {
-        this.id = pet.getId();
-        this.name = pet.getName();
-        this.age = pet.getAge();
-        this.breed = pet.getBreed();
-        this.medicalConditions = pet.getMedicalConditions();
-        this.profileImgUrl = pet.getProfileImgUrl();
+public class PetResponse {
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetList {
+        private long id;
+        private String name;
+        private int age;
+        private String breed;
+        private String medicalConditions;
+        private String profileImgUrl;
+
+        public GetList(Pet pet) {
+            this.id = pet.getId();
+            this.name = pet.getName();
+            this.age = pet.getAge();
+            this.breed = pet.getBreed();
+            this.medicalConditions = pet.getMedicalConditions();
+            this.profileImgUrl = pet.getProfileImgUrl();
+        }
     }
+
+    @NoArgsConstructor
+    @Getter
+    public static class GetReservation { // 고객이 예약 시 보여질 반려견 정보
+        private long id;
+        private String name;
+        private String breed;
+
+        public GetReservation(Pet pet) {
+            this.id = pet.getId();
+            this.name = pet.getName();
+            this.breed = pet.getBreed();
+        }
+    }
+
 }
