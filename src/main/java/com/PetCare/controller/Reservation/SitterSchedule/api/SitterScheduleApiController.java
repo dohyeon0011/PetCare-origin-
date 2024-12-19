e79bb208686920e75ae70cdaf70127a8ec6ff1d0
idohyeon <ehgus5026@naver.com>
2024-12-19 19:31:52 +0900

    -SitterSchedule API Controller(돌봄사 돌봄 예약 일정)

--- src/main/java/com/PetCare/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
+++ src/main/java/com/PetCare/controller/Reservation/SitterSchedule/api/SitterScheduleApiController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/PetsCare/sitter")
+@RequestMapping("/api/PetsCare/sitters")
 public class SitterScheduleApiController {
 
     private final SitterScheduleService sitterScheduleService;
