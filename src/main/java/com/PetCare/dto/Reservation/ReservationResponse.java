8e749ca4257568961855682f9919089a4cfba04c
idohyeon <ehgus5026@naver.com>
2024-12-19 19:34:40 +0900

    -Reservation View DTO(고객시점 예약할 시 보여질 정보)

--- src/main/java/com/PetCare/dto/Reservation/ReservationResponse.java
+++ src/main/java/com/PetCare/dto/Reservation/ReservationResponse.java
@@ -16,11 +16,15 @@ public class ReservationResponse { // 고객이 예약할 때 보여줄 정보
     private long sitterId;
     private String sitterName;
     private List<CareAvailableDateResponse.GetReservation> careAvailableDates;
+    private String zipcode;
+    private String address;
     private List<PetResponse.GetReservation> pets;
 
     public ReservationResponse(Member sitter, List<CareAvailableDate> careAvailableDates, List<Pet> pets) {
         this.sitterId = sitter.getId();
         this.sitterName = sitter.getName();
+        this.zipcode = sitter.getZipcode();
+        this.address = sitter.getAddress();
         this.careAvailableDates = careAvailableDates
                 .stream()
                 .map(CareAvailableDateResponse.GetReservation::new)
