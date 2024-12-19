ec09851c8ac71dbc9ae052aef262c716c7342371
idohyeon <ehgus5026@naver.com>
2024-12-19 19:37:00 +0900

    -Update Review DTO(돌봄 리뷰)

---
+++ src/main/java/com/PetCare/dto/Review/request/UpdateReviewRequest.java
@@ -0,0 +1,23 @@
+package com.PetCare.dto.Review.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateReviewRequest {
+
+    private long id;
+
+    @NotNull(message = "평점을 0 ~ 5점 사이로 입력하세요.")
+    @Min(value = 0, message = "평점은 최소 0점이어야 합니다.")
+    @Max(value = 5, message = "평점은 최대 5점이어야 합니다.")
+    private int rating;
+
+    private String comment;
+}
