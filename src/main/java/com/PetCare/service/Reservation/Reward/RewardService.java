0f42c8dd506625107c679a727d85b3cfcae3402b
idohyeon <ehgus5026@naver.com>
2024-12-19 19:36:34 +0900

    -돌봄 예약금 적립 추상 인터페이스

--- src/main/java/com/PetCare/service/Reservation/Reward/RewardService.java
+++ src/main/java/com/PetCare/service/Reservation/Reward/RewardService.java
@@ -1,6 +1,6 @@
 package com.PetCare.service.Reservation.Reward;
 
 public interface RewardService {
-    void addReward(long customerId, double amount); // 적립 메서드
-    double calculatorReward(double reservationAmount); // 적립금 계산 메서드
+    void addReward(long customerId, int amount); // 적립 메서드
+    int calculatorReward(int reservationAmount); // 적립금 계산 메서드
 }
