44c351e508d24d7586d74bce9e9a037adab62494
idohyeon <ehgus5026@naver.com>
2024-12-19 19:32:39 +0900

    -Add Review DTO(리뷰 작성)

---
+++ src/main/java/com/PetCare/dto/Review/request/AddReviewRequest.java
@@ -0,0 +1,34 @@
+package com.PetCare.dto.Review.request;
+
+import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
+import com.PetCare.domain.Review.Review;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddReviewRequest {
+
+    private long customerReservationId;
+
+    @NotNull(message = "평점을 0 ~ 5점 사이로 입력하세요.")
+    @Min(value = 0, message = "평점은 최소 0점이어야 합니다.")
+    @Max(value = 5, message = "평점은 최대 5점이어야 합니다.")
+    private int rating;
+
+    private String comment;
+
+    public Review toEntity(CustomerReservation customerReservation) {
+        return Review.builder()
+                .customerReservation(customerReservation)
+                .rating(rating)
+                .comment(comment)
+                .build();
+    }
+
+}
