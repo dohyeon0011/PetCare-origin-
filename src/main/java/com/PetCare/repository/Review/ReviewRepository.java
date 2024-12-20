112ea4a935a64c6159e62ae9e0872dbca51e8453
idohyeon <ehgus5026@naver.com>
2024-12-20 17:49:42 +0900

    -Review Repository(돌봄 예약 리뷰)

--- src/main/java/com/PetCare/repository/Review/ReviewRepository.java
+++ src/main/java/com/PetCare/repository/Review/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.PetCare.repository.Review;
 
+import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
 import com.PetCare.domain.Review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    boolean existsByCustomerReservation(CustomerReservation customerReservation);
 
     // 고객 시점 예약 엔티티의 customerId를 기준으로 모든 리뷰 조회
     List<Review> findByCustomerReservationCustomerId(long customerId);
