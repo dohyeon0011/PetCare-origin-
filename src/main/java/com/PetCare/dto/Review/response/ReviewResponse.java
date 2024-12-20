37cf79971219adcb641d2267c054c4a641466f89
idohyeon <ehgus5026@naver.com>
2024-12-20 17:49:50 +0900

    -Review View DTO(돌봄 예약 리뷰)

--- src/main/java/com/PetCare/dto/Review/response/ReviewResponse.java
+++ src/main/java/com/PetCare/dto/Review/response/ReviewResponse.java
@@ -8,25 +8,35 @@ public class ReviewResponse {
 
     @NoArgsConstructor
     @Getter
-    public static class GetList {
+    public static class GetList { // 모든 리뷰 조회할 때
         private long id;
-        private int rating;
+        private String customerNickName;
+        private String sitterName;
+        private Double rating;
 
         public GetList(Review review) {
             this.id = review.getId();
+            this.customerNickName = review.getCustomerReservation().getCustomer().getNickName();
+            this.sitterName = review.getCustomerReservation().getSitter().getName();
             this.rating = review.getRating();
         }
     }
 
     @NoArgsConstructor
     @Getter
-    public static class GetDetail {
+    public static class GetDetail { // 리뷰의 자세한 정보 조회
         private long id;
-        private int rating;
+        private long customerReservationId;
+        private String customerNickName;
+        private String sitterName;
+        private Double rating;
         private String comment;
 
         public GetDetail(Review review) {
             this.id = review.getId();
+            this.customerReservationId = review.getCustomerReservation().getId();
+            this.customerNickName = review.getCustomerReservation().getCustomer().getNickName();
+            this.sitterName = review.getCustomerReservation().getSitter().getName();
             this.rating = review.getRating();
             this.comment = review.getComment();
         }
