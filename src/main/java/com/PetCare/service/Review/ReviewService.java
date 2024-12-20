b3af959c46a49d26a3d8e436bf39d03397e0e1cb
idohyeon <ehgus5026@naver.com>
2024-12-20 17:50:01 +0900

    -Review Service(돌봄 예약 리뷰)

--- src/main/java/com/PetCare/service/Review/ReviewService.java
+++ src/main/java/com/PetCare/service/Review/ReviewService.java
@@ -28,20 +28,24 @@ public class ReviewService {
 
     @Comment("리뷰 작성")
     @Transactional
-    public Review saveReview(long customerId, AddReviewRequest request) {
+    public ReviewResponse.GetDetail save(long customerId, long reservationId, AddReviewRequest request) {
         Member customer = memberRepository.findById(customerId)
                 .orElseThrow(() -> new NoSuchElementException("로그인한 회원을 찾을 수 없습니다."));
 
         authorizationMember(customer);
         verifyingPermissionsCustomer(customer);
 
-        CustomerReservation customerReservation = customerReservationRepository.findById(request.getCustomerReservationId())
-                .orElseThrow(() -> new NoSuchElementException("해당 예약을 찾을 수 없습니다."));
+        CustomerReservation customerReservation = customerReservationRepository.findById(reservationId)
+                .orElseThrow(() -> new NoSuchElementException("리뷰 등록 실패: 해당 예약을 찾을 수 없습니다."));
+
+        if (reviewRepository.existsByCustomerReservation(customerReservation)) {
+            throw new IllegalArgumentException("이미 해당 예약에 대한 리뷰가 존재합니다.");
+        }
 
         Review review = request.toEntity(customerReservation);
         reviewRepository.save(review);
 
-        return review;
+        return review.toResponse();
     }
 
     @Comment("특정 회원의 작성한 리뷰 전체 조회")
@@ -57,17 +61,16 @@ public class ReviewService {
                 .toList();
     }
 
-    @Comment("특정 회원의 특정 리뷰 조회")
-    public ReviewResponse.GetDetail findById(long customerId, long reviewId) {
-        Member customer = memberRepository.findById(customerId)
-                .orElseThrow(() -> new NoSuchElementException("로그인한 회원을 찾을 수 없습니다."));
-
-        return reviewRepository.findByCustomerReservationCustomerIdAndId(customer.getId(), reviewId)
+    @Comment("특정 리뷰 조회")
+    @Transactional(readOnly = true)
+    public ReviewResponse.GetDetail findById(long reviewId) {
+        return reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new NoSuchElementException("해당 예약 건에 대한 리뷰를 찾을 수 없습니다."))
                 .toResponse();
     }
 
     @Comment("특정 회원의 특정 리뷰 삭제")
+    @Transactional
     public void delete(long customerId, long reviewId) {
         Member customer = memberRepository.findById(customerId)
                 .orElseThrow(() -> new NoSuchElementException("로그인한 회원을 찾을 수 없습니다."));
@@ -82,14 +85,15 @@ public class ReviewService {
     }
 
     @Comment("특정 회원의 특정 리뷰 수정")
-    public ReviewResponse.GetDetail update(long customerId, UpdateReviewRequest request) {
+    @Transactional
+    public ReviewResponse.GetDetail update(long customerId, long reviewId, UpdateReviewRequest request) {
         Member customer = memberRepository.findById(customerId)
                 .orElseThrow(() -> new NoSuchElementException("로그인한 회원을 찾을 수 없습니다."));
 
         authorizationMember(customer);
         verifyingPermissionsCustomer(customer);
 
-        Review review = reviewRepository.findByCustomerReservationCustomerIdAndId(customer.getId(), request.getId())
+        Review review = reviewRepository.findByCustomerReservationCustomerIdAndId(customer.getId(), reviewId)
                 .orElseThrow(() -> new NoSuchElementException("해당 리뷰를 찾을 수 없습니다."));
 
         review.updateReview(request.getRating(), request.getComment());
@@ -110,6 +114,5 @@ public class ReviewService {
             throw new IllegalArgumentException("고객만 리뷰 작성 및 수정,삭제가 가능합니다.");
         }
     }
-
 
 }
