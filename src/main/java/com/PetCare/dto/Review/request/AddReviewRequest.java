4cf1018ab779f72ef5d3c22b0847f82725b3bd9b
idohyeon <ehgus5026@naver.com>
2024-12-20 17:46:26 +0900

    -Add Review DTO(리뷰)

--- src/main/java/com/PetCare/dto/Review/request/AddReviewRequest.java
+++ src/main/java/com/PetCare/dto/Review/request/AddReviewRequest.java
@@ -14,12 +14,12 @@ import lombok.NoArgsConstructor;
 @Getter
 public class AddReviewRequest {
 
-    private long customerReservationId;
+//    private long customerReservationId; url에서 읽을거임
 
     @NotNull(message = "평점을 0 ~ 5점 사이로 입력하세요.")
     @Min(value = 0, message = "평점은 최소 0점이어야 합니다.")
     @Max(value = 5, message = "평점은 최대 5점이어야 합니다.")
-    private int rating;
+    private Double rating;
 
     private String comment;
 
