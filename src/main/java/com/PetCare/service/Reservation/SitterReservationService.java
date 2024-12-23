89a7a4cd0295d2468ab3eb17d72dfad1819f8567
idohyeon <ehgus5026@naver.com>
2024-12-23 18:20:27 +0900

    -SitterReservation Service(고객 시점 예약 전,후 보여질 정보)

--- src/main/java/com/PetCare/service/Reservation/SitterReservationService.java
+++ src/main/java/com/PetCare/service/Reservation/SitterReservationService.java
@@ -86,13 +86,13 @@ public class SitterReservationService {
 
     public static void verifyingPermissionsCustomer(Member customer) {
         if (!customer.getRole().equals(Role.CUSTOMER)) {
-            throw new IllegalArgumentException("고객만 예약 등록 및 수정,삭제가 가능합니다.");
+            throw new IllegalArgumentException("고객만 예약 등록,수정 및 삭제가 가능합니다.");
         }
     }
 
     public static void verifyingPermissionsSitter(Member sitter) {
         if (!sitter.getRole().equals(Role.PET_SITTER)) {
-            throw new IllegalArgumentException("돌봄 예약 배정 및 수정,삭제는 돌봄사만 가능합니다.");
+            throw new IllegalArgumentException("돌봄 예약 배정,수정 및 삭제는 돌봄사만 가능합니다.");
         }
     }
 
