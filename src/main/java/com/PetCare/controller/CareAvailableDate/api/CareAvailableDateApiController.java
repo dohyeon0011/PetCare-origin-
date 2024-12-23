d30c778360abfe06a898bd686f84523e34ee9d42
idohyeon <ehgus5026@naver.com>
2024-12-23 18:14:42 +0900

    -CareAvailableDate API Controller(돌봄사 돌봄 예약 가능 날짜)

--- src/main/java/com/PetCare/controller/CareAvailableDate/api/CareAvailableDateApiController.java
+++ src/main/java/com/PetCare/controller/CareAvailableDate/api/CareAvailableDateApiController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@RequestMapping("/api/pets-care/members")
 public class CareAvailableDateApiController {
 
     private final CareAvailableDateService careAvailableDateService;
