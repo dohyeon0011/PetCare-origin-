6100e0f3fecc315103937145ab52301432be03ed
idohyeon <ehgus5026@naver.com>
2024-12-23 18:21:14 +0900

    -Update CareLog DTO(돌봄 케어 로그)

---
+++ src/main/java/com/PetCare/dto/CareLog/request/UpdateCareLogRequest.java
@@ -0,0 +1,22 @@
+package com.PetCare.dto.CareLog.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateCareLogRequest {
+
+    @NotBlank(message = "케어 로그를 작성할 돌봄을 선택해야 합니다.")
+    private long sitterScheduleId;
+
+    private String careType;
+
+    @NotBlank(message = "해당 돌봄 케어에 대한 상세 설명을 필수로 적어야 합니다.")
+    private String description;
+
+    private String imgPath;
+}
