159a9e73a12ffe9a5a136d2187a43f58244db0d8
idohyeon <ehgus5026@naver.com>
2024-12-23 18:18:22 +0900

    -Pet API Controller(반려견)

--- src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
+++ src/main/java/com/PetCare/controller/Pet/api/PetApiController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@RequestMapping("/api/pets-care/members")
 public class PetApiController {
 
     private final PetService petService;
