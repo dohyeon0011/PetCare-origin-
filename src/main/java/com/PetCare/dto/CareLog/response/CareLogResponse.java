223ee086f8410db8ced95fd6305222e6a00a24df
idohyeon <ehgus5026@naver.com>
2024-12-24 17:27:57 +0900

    -CareLog View DTO(돌봄 케어 로그)

--- src/main/java/com/PetCare/dto/CareLog/response/CareLogResponse.java
+++ src/main/java/com/PetCare/dto/CareLog/response/CareLogResponse.java
@@ -6,23 +6,43 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@Getter
 public class CareLogResponse {
 
-    private long id;
-    private String sitterName; // 케어 로그 작성 돌봄자
-    private String careType;
-    private String description;
-    private String imgPath;
-    private LocalDateTime createdAt;
+    @NoArgsConstructor
+    @Getter
+    public static class GetList {
+        private long id;
+        private String sitterName; // 케어 로그 작성 돌봄자
+        private String careType;
+        private String description;
+        private LocalDateTime createdAt;
 
-    public CareLogResponse(CareLog careLog) {
-        this.sitterName = careLog.getSitterSchedule().getSitter().getName();
-        this.careType = careLog.getCareType();
-        this.description = careLog.getDescription();
-        this.imgPath = careLog.getImgPath();
-        this.createdAt = careLog.getCreatedAt();
+        public GetList(CareLog careLog) {
+            this.id = careLog.getId();
+            this.sitterName = careLog.getSitterSchedule().getSitter().getName();
+            this.careType = careLog.getCareType();
+            this.description = careLog.getDescription();
+            this.createdAt = careLog.getCreatedAt();
+        }
     }
 
+    @NoArgsConstructor
+    @Getter
+    public static class GetDetail {
+        private long id;
+        private String sitterName; // 케어 로그 작성 돌봄자
+        private String careType;
+        private String description;
+        private String imgPath;
+        private LocalDateTime createdAt;
+
+        public GetDetail(CareLog careLog) {
+            this.id = careLog.getId();
+            this.sitterName = careLog.getSitterSchedule().getSitter().getName();
+            this.careType = careLog.getCareType();
+            this.description = careLog.getDescription();
+            this.imgPath = careLog.getImgPath();
+            this.createdAt = careLog.getCreatedAt();
+        }
+    }
 }
