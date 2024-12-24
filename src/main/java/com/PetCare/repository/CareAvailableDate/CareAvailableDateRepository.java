e3bc022fb7e992f1a5fd4c816de85dae072d43de
idohyeon <ehgus5026@naver.com>
2024-12-24 17:26:32 +0900

    -CareAvailableDate Repository(돌봄사 돌봄 예약 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
@@ -1,11 +1,14 @@
 package com.PetCare.repository.CareAvailableDate;
 
 import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
+import com.PetCare.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface CareAvailableDateRepository extends JpaRepository<CareAvailableDate, Long> {
 
@@ -17,4 +20,8 @@ public interface CareAvailableDateRepository extends JpaRepository<CareAvailable
 
     // 특정 회원의 특정 날짜 조회
     Optional<CareAvailableDate> findBySitterIdAndAvailableAt(long sitterId, LocalDate availableAt);
+
+    // 돌봄 예약 가능 날짜를 등록한 회원 목록 조회
+    @Query("select distinct c.sitter from CareAvailableDate c")
+    Set<Member> findDistinctSitters();
 }
