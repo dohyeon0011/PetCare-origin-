a963c517e7fc214a73c5de4a4a4f67a57f446ad1
idohyeon <ehgus5026@naver.com>
2024-12-24 19:45:23 +0900

    -CareLog Repository(돌봄 케어 로그)

--- src/main/java/com/PetCare/repository/CareLog/CareLogRepository.java
+++ src/main/java/com/PetCare/repository/CareLog/CareLogRepository.java
@@ -3,5 +3,17 @@ package com.PetCare.repository.CareLog;
 import com.PetCare.domain.CareLog.CareLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CareLogRepository extends JpaRepository<CareLog, Long> {
+
+    // 돌봄사가 작성한 모든 돌봄 케어 로그 조회
+    List<CareLog> findAllBySitterScheduleSitterId(long sitterId);
+
+    // 돌봄사가 특정 돌봄에 대해 작성한 돌봄 케어 로그 조회
+    List<CareLog> findAllBySitterScheduleSitterIdAndSitterScheduleId(long sitterId, long sitterScheduleId);
+
+    // 돌봄사가 특정 돌봄에 대해 작성한 특정 돌봄 케어 로그 조회
+    Optional<CareLog> findBySitterScheduleSitterIdAndId(long sitterId, long careLogId);
 }
