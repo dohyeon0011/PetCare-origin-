4e0f6c5588e794893df167946a51a3110971f07f
idohyeon <ehgus5026@naver.com>
2024-12-26 17:00:19 +0900

    -CareAvailableDate Repository(돌봄사->돌봄 예약 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
@@ -1,16 +1,14 @@
 package com.PetCare.repository.CareAvailableDate;
 
 import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
-import com.PetCare.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-public interface CareAvailableDateRepository extends JpaRepository<CareAvailableDate, Long> {
+public interface CareAvailableDateRepository extends JpaRepository<CareAvailableDate, Long>, CareAvailableDateRepositoryCustom, QuerydslPredicateExecutor<CareAvailableDate> {
 
     // 특정 회원의 돌봄 가능한 날짜 모두 조회
     List<CareAvailableDate> findBySitterId(long sitterId);
@@ -22,6 +20,6 @@ public interface CareAvailableDateRepository extends JpaRepository<CareAvailable
     Optional<CareAvailableDate> findBySitterIdAndAvailableAt(long sitterId, LocalDate availableAt);
 
     // 돌봄 예약 가능 날짜를 등록한 회원 목록 조회
-    @Query("select distinct c.sitter from CareAvailableDate c")
-    Set<Member> findDistinctSitters();
+//    @Query("select distinct c.sitter from CareAvailableDate c")
+//    Set<Member> findDistinctSitters();
 }
