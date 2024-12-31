70464f0e21b20b53eba88a910902182e1242193a
idohyeon <ehgus5026@naver.com>
2024-12-31 15:57:17 +0900

    -Review View Controller(돌봄 리뷰)

---
+++ src/main/java/com/PetCare/controller/Review/view/ReviewViewController.java
@@ -0,0 +1,50 @@
+package com.PetCare.controller.Review.view;
+
+import com.PetCare.dto.Review.response.ReviewResponse;
+import com.PetCare.service.Review.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pets-care")
+public class ReviewViewController {
+
+    private final ReviewService reviewService;
+
+    @Operation(description = "리뷰 작성")
+    @GetMapping("/members/{customerId}/reservations/{customerReservationId}/reviews/new")
+    public String newReview(@PathVariable("customerId") long customerId,
+                            @PathVariable("customerReservationId") long customerReservationId, Model model) {
+        ReviewResponse.GetNewReview response = reviewService.getNewReview(customerId, customerReservationId);
+        model.addAttribute("response", response);
+
+        return "review/newReview";
+    }
+
+    @Operation(description = "특정 회원의 모든 리뷰 조회")
+    @GetMapping("/members/{customerId}/reviews")
+    public String getAllReview(@PathVariable("customerId") long customerId, Model model) {
+        List<ReviewResponse.GetList> reviews = reviewService.findAllById(customerId);
+        model.addAttribute("reviews", reviews);
+
+        return "review/reviewList";
+    }
+
+    @Operation(description = "특정 리뷰 조회")
+    @GetMapping("/reviews/{reviewId}")
+    public String getReview(@PathVariable("reviewId") long reviewId, Model model) {
+        ReviewResponse.GetDetail review = reviewService.findById(reviewId);
+        model.addAttribute("review", review);
+
+        return "review/reviewDetail";
+    }
+
+}
