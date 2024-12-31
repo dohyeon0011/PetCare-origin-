9ede92b53acc65dc614c233c531cfb3ec0b70d1b
idohyeon <ehgus5026@naver.com>
2024-12-31 15:58:01 +0900

    -SitterSchedule View Controller(돌봄사 시점->돌봄 예약)

---
+++ src/main/java/com/PetCare/controller/Reservation/SitterSchedule/view/SitterScheduleViewController.java
@@ -0,0 +1,41 @@
+package com.PetCare.controller.Reservation.SitterSchedule.view;
+
+import com.PetCare.dto.Reservation.SitterSchedule.response.SitterScheduleResponse;
+import com.PetCare.service.Reservation.SitterSchedule.SitterScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pets-care")
+public class SitterScheduleViewController {
+
+    private final SitterScheduleService sitterScheduleService;
+
+    @Operation(description = "돌봄사의 전체 돌봄 예약 목록 조회")
+    @GetMapping("/members/{sitterId}/schedules")
+    public String getAllSitterSchedule(@PathVariable("sitterId") long sitterId, Model model) {
+        List<SitterScheduleResponse.GetList> schedules = sitterScheduleService.findAllById(sitterId);
+        model.addAttribute("schedules", schedules);
+
+        return "schedule/scheduleList";
+    }
+
+    @Operation(description = "특정 돌봄사의 특정 돌봄 예약 정보 조회 API")
+    @GetMapping("/members/{sitterId}/schedules/{sitterScheduleId}")
+    public String getSitterSchedule(@PathVariable("sitterId") long sitterId,
+                                    @PathVariable("sitterScheduleId") long sitterScheduleId, Model model) {
+        SitterScheduleResponse.GetDetail schedule = sitterScheduleService.findById(sitterId, sitterScheduleId);
+        model.addAttribute("schedule", schedule);
+
+        return "schedule/scheduleDetail";
+    }
+
+}
