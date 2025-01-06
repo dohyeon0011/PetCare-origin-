4921ed8938d027ba23c1b06169b9a71d3c3121f9
idohyeon <ehgus5026@naver.com>
2025-01-06 16:05:19 +0900

    -CareAvailableDate Repository(돌봄사 돌봄 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
@@ -1,8 +1,13 @@
 package com.PetCare.repository.CareAvailableDate;
 
 import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
+import com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,6 +23,13 @@ public interface CareAvailableDateRepository extends JpaRepository<CareAvailable
 
     // 특정 회원의 특정 날짜 조회
     Optional<CareAvailableDate> findBySitterIdAndAvailableAt(long sitterId, LocalDate availableAt);
+
+    // 자신이 등록한 돌봄 가능 날짜 조회 페이징
+//    @Query("SELECT new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c) " +
+//        "FROM CareAvailableDate c WHERE c.sitter.id = :sitterId")
+    @Query("SELECT new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c.id, c.availableAt, c.price, c.status) " +
+            "FROM CareAvailableDate c WHERE c.sitter.id = :sitterId")
+    Page<CareAvailableDateResponse.GetList> findBySitterId(@Param("sitterId") long sitterId, Pageable pageable);
 
     // 돌봄 예약 가능 날짜를 등록한 회원 목록 조회
 //    @Query("select distinct c.sitter from CareAvailableDate c")
