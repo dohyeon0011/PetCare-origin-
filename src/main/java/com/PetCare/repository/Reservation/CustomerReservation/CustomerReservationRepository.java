c8153b06a91e6f02dba52635990a06990558ac68
idohyeon <ehgus5026@naver.com>
2025-01-06 18:44:56 +0900

    -CustomerReservation Repository(돌봄 예약->고객 시점)

--- src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
+++ src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
@@ -1,7 +1,12 @@
 package com.PetCare.repository.Reservation.CustomerReservation;
 
 import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
+import com.PetCare.dto.Reservation.CustomerReservation.response.CustomerReservationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +18,11 @@ public interface CustomerReservationRepository extends JpaRepository<CustomerRes
 
     // 고객의 번호로 특정 회원의 특정 돌봄 예약 내역 조회
     Optional<CustomerReservation> findByCustomerIdAndId(long customerId, long id);
+
+    // 특정 고객의 돌봄 예약 내역 전체 조회(+페이징)
+    @Query("SELECT new com.PetCare.dto.Reservation.CustomerReservation.response.CustomerReservationResponse$GetList(c) " +
+            "FROM CustomerReservation c WHERE c.customer.id = :customerId")
+    Page<CustomerReservationResponse.GetList> findByCustomerId(@Param("customerId") long customerId, Pageable pageable);
 
     // 돌봄사의 예약 번호로 특정 회원의 특정 돌봄 예약 내역 조회
 //    Optional<CustomerReservation> findBySitterScheduleId(long sitterScheduleId);
