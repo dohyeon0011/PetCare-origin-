479824a8fb108830f0118a72fee0063ecfc905c5
idohyeon <ehgus5026@naver.com>
2025-01-07 23:58:53 +0900

    -Review Repository(돌봄 리뷰)

--- src/main/java/com/PetCare/repository/Review/ReviewRepository.java
+++ src/main/java/com/PetCare/repository/Review/ReviewRepository.java
@@ -16,13 +16,18 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByCustomerReservation(CustomerReservation customerReservation);
 
-    // 고객 시점 예약 엔티티의 customerId를 기준으로 모든 리뷰 조회
+    // 고객 시점 예약 엔티티의 customerId(고객)를 기준으로 모든 리뷰 조회
     List<Review> findByCustomerReservationCustomerId(long customerId);
 
-    // 고객 시점 예약 엔티티의 customerId를 기준으로 모든 리뷰 조회(+페이징)
+    // 고객 시점 예약 엔티티의 customerId(고객)를 기준으로 모든 리뷰 조회(+페이징)
     @Query("SELECT new com.PetCare.dto.Review.response.ReviewResponse$GetList(r) " +
             "FROM Review r WHERE r.customerReservation.customer.id = :customerId")
     Page<ReviewResponse.GetList> findByCustomerReservationCustomerId(@Param("customerId") long customerId, Pageable pageable);
+
+    // 고객 시점 예약 엔티티의 sitterId(돌봄사)를 기준으로 모든 리뷰 조회(+페이징)
+    @Query("SELECT new com.PetCare.dto.Review.response.ReviewResponse$GetList(r) " +
+            "FROM Review r WHERE r.customerReservation.sitter.id = :sitterId")
+    Page<ReviewResponse.GetList> findByCustomerReservationSitterId(@Param("sitterId") long sitterId, Pageable pageable);
 
     // 고객 시점 예약 엔티티의 sitterId를 기준으로 모든 리뷰 조회
     List<Review> findByCustomerReservationSitterId(long sitterId);
