f3db49265be979d9a11f9ea914a0e433096b1441
idohyeon <ehgus5026@naver.com>
2025-01-11 18:30:20 +0900

    -SitterScheduleRepository Custom(돌봄사->돌봄 일정)

--- src/main/java/com/PetCare/repository/Reservation/SitterSchedule/SitterScheduleRepositoryCustom.java
+++ src/main/java/com/PetCare/repository/Reservation/SitterSchedule/SitterScheduleRepositoryCustom.java
@@ -8,4 +8,7 @@ public interface SitterScheduleRepositoryCustom {
 
     // 돌봄사에게 배정됐던 모든 예약 조회(+페이징)
     Page<SitterScheduleResponse.GetList> findBySitterId(long sitterId, Pageable pageable);
+
+    // 특정 돌봄사의 특정 돌봄 예약 정보 조회(+DTO로 직접 조회)
+//    SitterScheduleResponse.GetDetail findSitterScheduleDetail(long sitterId, long sitterScheduleId);
 }
