084e8ef2f7df7ccc018b414a537378d488f6509c
idohyeon <ehgus5026@naver.com>
2025-01-13 19:35:27 +0900

    -CareAvailableDate Repository Custom(돌봄사->돌봄 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepositoryCustom.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepositoryCustom.java
@@ -2,14 +2,18 @@ package com.PetCare.repository.CareAvailableDate;
 
 import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
 import com.PetCare.domain.Member.Member;
+import com.PetCare.dto.Reservation.ReservationSitterResponse;
 
 import java.util.List;
 import java.util.Set;
 
 public interface CareAvailableDateRepositoryCustom {
 
-    // 돌봄 예약 가능 날짜를 등록한 회원 중 현재 예약이 가능한 회원만 조회
+    // 돌봄 예약 가능 날짜를 등록한 회원 중 현재 예약이 가능한 회원만 조회(+엔티티로 조회 후 DTO 변환)
     Set<Member> findDistinctSitters();
+
+    // 돌봄 예약 가능 날짜를 등록한 회원 중 현재 예약이 가능한 회원만 조회(+DTO로 직접 조회)
+    Set<ReservationSitterResponse.GetList> findDistinctSitterDetail();
 
     // 특정 돌봄사의 돌봄 예약 가능한 날짜만을 조회
     List<CareAvailableDate> findBySitterIdAndPossibility(long sitterId);
