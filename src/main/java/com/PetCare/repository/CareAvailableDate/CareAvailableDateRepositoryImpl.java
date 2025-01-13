1038b149d967697bbe26b06fc8baf82182a6221d
idohyeon <ehgus5026@naver.com>
2025-01-13 19:35:32 +0900

    -CareAvailableDate Repository Impl(돌봄사->돌봄 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepositoryImpl.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.PetCare.repository.CareAvailableDate;
 import com.PetCare.domain.CareAvailableDate.CareAvailableDate;
 import com.PetCare.domain.CareAvailableDate.CareAvailableDateStatus;
 import com.PetCare.domain.Member.Member;
+import com.PetCare.dto.Reservation.QReservationSitterResponse_GetList;
+import com.PetCare.dto.Reservation.ReservationSitterResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +24,17 @@ public class CareAvailableDateRepositoryImpl implements CareAvailableDateReposit
     public Set<Member> findDistinctSitters() {
         return queryFactory
                 .select(careAvailableDate.sitter).distinct()
+                .from(careAvailableDate)
+                .where(careAvailableDate.status.eq(CareAvailableDateStatus.POSSIBILITY))
+                .stream()
+                .collect(Collectors.toSet());
+    }
+
+    // 돌봄 예약 가능 날짜를 등록한 회원 중 현재 예약이 가능한 회원만 조회(+DTO로 직접 조회)
+    @Override
+    public Set<ReservationSitterResponse.GetList> findDistinctSitterDetail() {
+        return queryFactory
+                .select(new QReservationSitterResponse_GetList(careAvailableDate.sitter.id, careAvailableDate.sitter.name, careAvailableDate.sitter.introduction)).distinct()
                 .from(careAvailableDate)
                 .where(careAvailableDate.status.eq(CareAvailableDateStatus.POSSIBILITY))
                 .stream()
