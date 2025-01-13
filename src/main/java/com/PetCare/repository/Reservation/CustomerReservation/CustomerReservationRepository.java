a76b8452007007855499f6b1b20135e14daa1926
idohyeon <ehgus5026@naver.com>
2025-01-13 19:35:45 +0900

    -CareAvailableDate Repository(돌봄사->돌봄 가능 날짜)

--- src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
+++ src/main/java/com/PetCare/repository/Reservation/CustomerReservation/CustomerReservationRepository.java
@@ -2,6 +2,7 @@ package com.PetCare.repository.Reservation.CustomerReservation;
 
 import com.PetCare.domain.Reservation.CustomerReservation.CustomerReservation;
 import com.PetCare.dto.Reservation.CustomerReservation.response.CustomerReservationResponse;
+import com.PetCare.dto.Review.response.ReviewResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,15 +17,22 @@ public interface CustomerReservationRepository extends JpaRepository<CustomerRes
     // 특정 고객의 돌봄 예약 내역 전체 조회
     List<CustomerReservation> findByCustomerId(long customerId);
 
-    // 고객의 번호로 특정 회원의 특정 돌봄 예약 내역 조회
+    // 고객의 번호로 특정 회원의 특정 돌봄 예약 내역 조회(+엔티티로 직접 조회 후 DTO로 변환)
     Optional<CustomerReservation> findByCustomerIdAndId(long customerId, long id);
+
+    // 고객의 번호로 특정 회원의 특정 돌봄 예약 내역 조회(+DTO로 직접 조회)
+    @Query("SELECT new com.PetCare.dto.Review.response.ReviewResponse$GetNewReview(" +
+            "cr.id, c.nickName, s.name) " +
+            "FROM CustomerReservation cr " +
+            "JOIN cr.customer c " +
+            "JOIN cr.sitter s " +
+            "WHERE cr.id = :id AND c.id = :customerId")
+    Optional<ReviewResponse.GetNewReview> findReviewResponseDetail(@Param("customerId") long customerId, @Param("id") long id);
 
     // 특정 고객의 돌봄 예약 내역 전체 조회(+페이징)
     @Query("SELECT new com.PetCare.dto.Reservation.CustomerReservation.response.CustomerReservationResponse$GetList(c) " +
             "FROM CustomerReservation c WHERE c.customer.id = :customerId")
     Page<CustomerReservationResponse.GetList> findByCustomerId(@Param("customerId") long customerId, Pageable pageable);
-
-
 
     // 돌봄사의 예약 번호로 특정 회원의 특정 돌봄 예약 내역 조회
 //    Optional<CustomerReservation> findBySitterScheduleId(long sitterScheduleId);
