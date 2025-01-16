085a4c68c75f3e6723c11a5d8f0050811cbbe4f7
idohyeon <ehgus5026@naver.com>
2025-01-16 17:28:19 +0900

    -CareAvailableData Repository(돌봄사->돌봄 가능 날짜)

--- src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
+++ src/main/java/com/PetCare/repository/CareAvailableDate/CareAvailableDateRepository.java
@@ -22,21 +22,21 @@ public interface CareAvailableDateRepository extends JpaRepository<CareAvailable
     Optional<CareAvailableDate> findBySitterIdAndId(long sitterId, long id);
 
     // 특정 회원의 특정 돌봄 가능한 날짜 DTO로 직접 조회
-    @Query("SELECT new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetDetail(" +
+    @Query("select new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetDetail(" +
             "c.id, c.availableAt, c.price, s.zipcode, s.address, c.status) " +
-            "FROM CareAvailableDate c " +
-            "JOIN c.sitter s " +
-            "WHERE s.id = :sitterId AND c.id = :id")
+            "from CareAvailableDate c " +
+            "join c.sitter s " +
+            "where s.id = :sitterId AND c.id = :id")
     Optional<CareAvailableDateResponse.GetDetail> findBySitterIdAndIdDetail(@Param("sitterId") long sitterId, @Param("id") long id);
 
     // 특정 회원의 특정 날짜 조회
     Optional<CareAvailableDate> findBySitterIdAndAvailableAt(long sitterId, LocalDate availableAt);
 
     // 자신이 등록한 돌봄 가능 날짜 조회 페이징
-//    @Query("SELECT new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c) " +
-//        "FROM CareAvailableDate c WHERE c.sitter.id = :sitterId")
-    @Query("SELECT new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c.id, c.availableAt, c.price, c.status) " +
-            "FROM CareAvailableDate c WHERE c.sitter.id = :sitterId")
+//    @Query("select new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c) " +
+//        "from CareAvailableDate c where c.sitter.id = :sitterId")
+    @Query("select new com.PetCare.dto.CareAvailableDate.response.CareAvailableDateResponse$GetList(c.id, c.availableAt, c.price, c.status) " +
+            "from CareAvailableDate c where c.sitter.id = :sitterId")
     Page<CareAvailableDateResponse.GetList> findBySitterId(@Param("sitterId") long sitterId, Pageable pageable);
 
     // 돌봄 예약 가능 날짜를 등록한 회원 목록 조회
