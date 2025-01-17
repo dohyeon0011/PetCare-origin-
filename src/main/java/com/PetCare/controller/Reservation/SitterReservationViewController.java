a740ff528e8f47d603429560c6089a633de52512
idohyeon <ehgus5026@naver.com>
2025-01-17 22:47:37 +0900

    -SitterReservation VIEW Controller(고객시점->예약하기 전 조회될 정보)

--- src/main/java/com/PetCare/controller/Reservation/SitterReservationViewController.java
+++ src/main/java/com/PetCare/controller/Reservation/SitterReservationViewController.java
@@ -4,14 +4,14 @@ import com.PetCare.dto.Reservation.ReservationSitterResponse;
 import com.PetCare.service.Reservation.SitterReservationService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,8 +22,9 @@ public class SitterReservationViewController {
 
     @Operation(description = "고객에게 돌봄 예약 가능한 돌봄사들의 정보 조회")
     @GetMapping("/reservableList")
-    public String getAllReservable(Model model) {
-        List<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters();
+    public String getAllReservable(Pageable pageable, Model model) {
+//        List<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters();
+        Page<ReservationSitterResponse.GetList> reservableSitters = sitterReservationService.findReservableSitters(pageable);
         model.addAttribute("reservableSitters", reservableSitters);
 
         return "reservable/reservableList";
