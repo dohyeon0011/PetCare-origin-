f968e477d935d64428a8bbc1643bfaec4dc86788
idohyeon <ehgus5026@naver.com>
2025-01-22 23:12:28 +0900

    -Review Repository Custom(돌봄 리뷰)

--- src/main/java/com/PetCare/repository/Review/ReviewRepositoryCustom.java
+++ src/main/java/com/PetCare/repository/Review/ReviewRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.PetCare.repository.Review;
 
 import com.PetCare.domain.Review.Review;
+import com.PetCare.dto.Review.response.ReviewResponse;
 
 import java.util.List;
 
@@ -8,4 +9,7 @@ public interface ReviewRepositoryCustom {
 
     // 고객 시점 예약 엔티티의 sitterId를 기준으로 모든 리뷰 조회(+페이징)
     List<Review> findBySitterId(long sitterId, int page, int size);
+
+    // 모든 리뷰 조회(+최신 6개만)
+    List<ReviewResponse.GetDetail> findAllReview();
 }
