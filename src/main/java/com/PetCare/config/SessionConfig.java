f05f8df8068b6a42d425a42f89102e2c65c6a39f
idohyeon <ehgus5026@naver.com>
2025-01-24 19:05:58 +0900

    -Session Config

---
+++ src/main/java/com/PetCare/config/SessionConfig.java
@@ -0,0 +1,16 @@
+/*
+package com.PetCare.config;
+
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SessionConfig implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        se.getSession().setMaxInactiveInterval(30 * 60); // 30분
+    }
+}
+*/
