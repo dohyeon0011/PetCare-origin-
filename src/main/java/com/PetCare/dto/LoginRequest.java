a29db6e49c85a63b8eb954bb08f9fd1d12854327
idohyeon <ehgus5026@naver.com>
2025-01-28 18:33:19 +0900

    -Login Request DTO

---
+++ src/main/java/com/PetCare/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.PetCare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class LoginRequest {
+
+    private String loginId;
+    private String password;
+}
