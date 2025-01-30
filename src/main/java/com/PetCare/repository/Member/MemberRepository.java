435ecd1375f9209aa902cf78243028e6cf1fe8e9
idohyeon <ehgus5026@naver.com>
2025-01-30 23:38:35 +0900

    -Member Repository(회원)

--- src/main/java/com/PetCare/repository/Member/MemberRepository.java
+++ src/main/java/com/PetCare/repository/Member/MemberRepository.java
@@ -1,12 +1,32 @@
 package com.PetCare.repository.Member;
 
 import com.PetCare.domain.Member.Member;
+import com.PetCare.domain.Member.Role;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
-    List<Member> findByLoginId(String loginId);
+    Optional<Member> findByLoginId(String loginId);
+
+    /*@Query("select new com.PetCare.dto.Member.response.MemberResponse$GetSitter(" +
+            "s.id, s.name, s.careerYear, s.certifications")
+    List<MemberResponse.GetSitter> findAllSitter();*/
+
+//    @EntityGraph(attributePaths = {"pets"})
+//    @Query("select m from Member m where m.id = :id and m.role = :role")
+    @Query("select m from Member m join fetch m.pets where m.id = :id and m.role = :role")
+    Optional<Member> findByCustomerId(@Param("id") long id, @Param("role") Role role);
+
+    @EntityGraph(attributePaths = {"certifications"})
+    @Query("select m from Member m where m.id = :id and m.role = :role")
+    Optional<Member> findBySitterId(@Param("id") long id, @Param("role") Role role);
+
+    @Query("select m.role from Member m where m.id = :id")
+    Optional<Role> findRoleById(@Param("id") long id);
 }
