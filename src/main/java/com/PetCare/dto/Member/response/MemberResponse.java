92e5dcf6d1bf27ecf19f509f4cb90e02e26a6b5c
idohyeon <ehgus5026@naver.com>
2025-02-01 18:14:32 +0900

    -Member View DTO(회원)

--- src/main/java/com/PetCare/dto/Member/response/MemberResponse.java
+++ src/main/java/com/PetCare/dto/Member/response/MemberResponse.java
@@ -1,18 +1,10 @@
 package com.PetCare.dto.Member.response;
 
-import com.PetCare.domain.Certification.Certification;
 import com.PetCare.domain.Member.Member;
 import com.PetCare.domain.Member.Role;
 import com.PetCare.domain.Member.SocialProvider;
-import com.PetCare.domain.Pet.Pet;
-import com.PetCare.dto.Certification.response.CertificationResponse;
-import com.PetCare.dto.Pet.response.PetResponse;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class MemberResponse {
@@ -21,7 +13,6 @@ public class MemberResponse {
     @Getter
     public static class GetCustomer {
         private long id;
-        private String password;
         private String name;
         private String nickName;
         private String email;
@@ -32,11 +23,10 @@ public class MemberResponse {
         private SocialProvider socialProvider;
         private String introduction;
         private int amount;
-        private List<PetResponse.GetList> pets = new ArrayList<>();
+//        private List<PetResponse.GetList> pets = new ArrayList<>();
 
-        public GetCustomer(Member member, List<Pet> pets) {
+        public GetCustomer(Member member) {
             this.id = member.getId();
-            this.password = member.getPassword();
             this.name = member.getName();
             this.nickName = member.getNickName();
             this.email = member.getEmail();
@@ -47,9 +37,9 @@ public class MemberResponse {
             this.socialProvider = member.getSocialProvider();
             this.introduction = member.getIntroduction();
             this.amount = member.getAmount();
-            this.pets = pets.stream()
-                    .map(PetResponse.GetList::new)
-                    .collect(Collectors.toList());
+//            this.pets = pets.stream()
+//                    .map(PetResponse.GetList::new)
+//                    .collect(Collectors.toList());
         }
     }
 
@@ -57,7 +47,6 @@ public class MemberResponse {
     @Getter
     public static class GetSitter {
         private long id;
-        private String password;
         private String name;
         private String nickName;
         private String email;
@@ -68,11 +57,10 @@ public class MemberResponse {
         private SocialProvider socialProvider;
         private String introduction;
         private Integer careerYear;
-        private List<CertificationResponse.GetList> certifications;
+//        private List<CertificationResponse.GetList> certifications;
 
-        public GetSitter(Member member, List<Certification> certifications) {
+        public GetSitter(Member member) {
             this.id = member.getId();
-            this.password = member.getPassword();
             this.name = member.getName();
             this.nickName = member.getNickName();
             this.email = member.getEmail();
@@ -83,9 +71,9 @@ public class MemberResponse {
             this.socialProvider = member.getSocialProvider();
             this.introduction = member.getIntroduction();
             this.careerYear = member.getCareerYear();
-            this.certifications = certifications.stream()
-                    .map(CertificationResponse.GetList::new)
-                    .collect(Collectors.toList());
+//            this.certifications = certifications.stream()
+//                    .map(CertificationResponse.GetList::new)
+//                    .collect(Collectors.toList());
         }
     }
 
